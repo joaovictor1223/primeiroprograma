5fee4cded6c3597d268072415556e25056b20617
aluno <aluno@192.168.0.66>
2024-11-27 16:38:22 -0300

    Segundo Commit

--- primeiroprograma/src/primeiroprograma/Executavel.java
+++ primeiroprograma/src/primeiroprograma/Executavel.java
@@ -5,7 +5,20 @@ public class Executavel {
 	public static void main(String[] args) {
 		System.out.println("Olá Mundo!");
 		System.out.println("Meu nome é João");
-		System.out.println("Tenho 15 anos");
+		//Criei variáveis, decimal e Texto.
+		int inteiro;
+		double numeroDecimal;
+		String texto;
+		
+		//Atribuição de valores as variáveis
+		inteiro = 5;
+		numeroDecimal = 5.65;
+        texto = "João";
+        
+        System.out.println("Nome: " + texto);
+        System.out.println("Numero inteiro: " + inteiro);
+        System.out.println("Numero decimal: " + numeroDecimal);
+        
 	}
 
 }
